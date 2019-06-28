03f82334d0f23ceccdc23a32be372692993cce35
Ihab Ali <ihab@live.com>
2019-06-28 08:13:27 -0500

    working on login

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginForm from "./components/LoginForm";
+import Navbar from "./components/Navbar";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleLogin = state => {
+    const baseUrl = "http://localhost:5000";
+    axios
+      .post(`${baseUrl}/api/login`, state)
+      .then(res => console.log(res.data.payload))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <Router>
+        <Navbar />
+        <Route
+          path="/"
+          render={props => (
+            <LoginForm {...props} handleLogin={this.handleLogin} />
+          )}
+        />
+      </Router>
+    );
+  }
 }
 
 export default App;
